e8321e5fdad9c60723d91f5c6d9069c7fd96fe55
ats <atsushi.k214@gmail.com>
2025-10-03 01:10:07 +0900

    useGroupsQuery関数のstaleTimeを10分に設定し、データの新鮮さを向上

--- src/features/dashboard/useGroupsQuery.ts
+++ src/features/dashboard/useGroupsQuery.ts
@@ -25,6 +25,6 @@ export function useGroupsQuery({ date, range }: Params) {
     queryFn: () => fetchGroupsByRange({ date: date!, timeRange: range! }),
     enabled,
     placeholderData: (previous) => previous,
-    staleTime: 60 * 1000,
+    staleTime: 1000 * 60 * 10,
   });
 }
